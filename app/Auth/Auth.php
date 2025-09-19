f4b66c4192ac23e48acd0207abc47cd66cfd5cc9
Araelka <RoseRedOwl@gmail.com>
2025-09-19 15:34:41 +0700

    modified:   app/Auth/Auth.php

--- app/Auth/Auth.php
+++ app/Auth/Auth.php
@@ -56,7 +56,9 @@ class Auth {
     }
 
     public function check(){
-        session_start();
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
+        }
         return isset($_SESSION['user_id']);
     }
 
